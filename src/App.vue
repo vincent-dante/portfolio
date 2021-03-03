3220f3f6b8dab933e5b89e191c2fce03f2be1022
Vincent Dante <vincent.dante96@gmail.com>
2021-03-04 02:09:53 +0800

    update design

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,100 @@
 <template>
-<!--   <div id="nav">
-    
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div> -->
-  <router-view/>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <div class="left-container border-end">
+          <div class="thumbnail-container">
+            <img src="./assets/pfpics.jpeg" alt="" srcset="" class="rounded-circle shadow">
+          </div>
+          <h1 class="title-name">Vincent Dante</h1>
+          <div class="link-container">
+            <router-link to="/" class="a-link">Home</router-link>
+            <router-link to="/" class="a-link">About</router-link>
+            <router-link to="/" class="a-link">Education</router-link>
+            <router-link to="/" class="a-link">Projects</router-link>
+            <router-link to="/" class="a-link">Experience</router-link>
+          </div>
+          
+          <div class="left-container-icon">
+            <i class="fab fa-2x fa-github media-icon"></i>
+            <i class="fab fa-2x fa-linkedin media-icon"></i>
+            <i class="fab fa-2x fa-facebook media-icon"></i>
+          </div>       
+        </div>
+      </div>
+      <div class="col-lg-9">
+        <!-- add transition here - ongoing -->
+        <router-view/>
+      </div>
+    </div>
+  </div>
+
+
+  
 </template>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.left-container {
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  padding-top: 50px;
+
+  .thumbnail-container {
+    height: 150px;
+
+    img {
+      height: 100%;
+    }
+  }
+
+  .title-name {
+    font-size: 24px;
+    font-family: 'Nothing You Could Do', cursive;
+    padding-top: 30px;
+    margin: 0;
+  }
+
+  .link-container {
+    display: grid;
+    justify-content: center;
+    padding: 50px 0;
+
+    a {
+      text-decoration: none;
+      text-align: left;
+      display: block;
+      font-size: 16px;
+      color: #2c3e50;
+      padding: 10px 0;
+    }
+  }
+
+  .left-container-icon {
+
+    .media-icon {
+      padding: 10px;
+      cursor: pointer;
+    }
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.left-container::after {
+  content: "";
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  right: -8px;
+  background: #fff;
+  border: 1px solid #d5d5d5;
+  border-radius: 50%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,29 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3">
-        <div class="left-container border-end">
-          <div class="thumbnail-container">
-            <img src="../assets/pfpics.jpeg" alt="" srcset="" class="rounded-circle shadow">
-          </div>
-          <div class="link-container">
-            <router-link to="/" class="a-link">Home</router-link>
-            <router-link to="/" class="a-link">About</router-link>
-            <router-link to="/" class="a-link">Education</router-link>
-            <router-link to="/" class="a-link">Projects</router-link>
-            <router-link to="/" class="a-link">Experience</router-link>
-          </div>
-          
-          <div class="left-container-icon">
-            <i class="fab fa-2x fa-github media-icon"></i>
-            <i class="fab fa-2x fa-linkedin media-icon"></i>
-            <i class="fab fa-2x fa-facebook media-icon"></i>
-          </div>       
-        </div>
-      </div>
-      <div class="col-lg-9">
-        <div class="right-container">
-          <div class="page-title-container border-bottom">
-            <h1 class="page-title">Home</h1>
-          </div>
-          
-          <p><span class="welcome-msg">Hi there welcome to my portfolio!</span></p>
-
-          <p>
-            I'm a web developer and designer with a degree of Bachelor of Science in Information Technology.
-            I always love to create and learn awesome stuff.
-          </p>
-          
-
-
-          <br><br><br><br><br><br><br>
-
-          <h2 class="sub-heading">Technology</h2>
-
-          <i class="fab fa-3x fa-js programming-icon"></i>
-          <i class="fab fa-3x fa-node-js programming-icon"></i>
-          <i class="fab fa-3x fa-vuejs programming-icon"></i>
-          <i class="fab fa-3x fa-html5 programming-icon"></i>
-          <i class="fab fa-3x fa-css3 programming-icon"></i>
-          <i class="fab fa-3x fa-laravel programming-icon"></i>
-          <i class="fab fa-3x fa-php programming-icon"></i>
-
-          
-        </div>
-      </div>
+  <div class="right-container">
+    <div class="page-title-container border-bottom">
+      <h1 class="page-title">Home</h1>
     </div>
+    
+    <p><span class="welcome-msg">Hi there welcome to my portfolio!</span></p>
+
+    <p>
+      I'm a web developer and designer with a degree of Bachelor of Science in Information Technology.
+      I always love to create and learn awesome stuff.
+    </p>
+    
+
+
+    <br><br><br><br><br><br><br>
+
+    <h2 class="sub-heading">Technology</h2>
+
+    <i class="fab fa-3x fa-js programming-icon"></i>
+    <i class="fab fa-3x fa-node-js programming-icon"></i>
+    <i class="fab fa-3x fa-vuejs programming-icon"></i>
+    <i class="fab fa-3x fa-html5 programming-icon"></i>
+    <i class="fab fa-3x fa-css3 programming-icon"></i>
+    <i class="fab fa-3x fa-laravel programming-icon"></i>
+    <i class="fab fa-3x fa-php programming-icon"></i>
 
     
   </div>
@@ -64,46 +36,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.left-container {
-  position: -webkit-sticky; /* Safari */
-  position: sticky;
-  top: 0;
-  padding-top: 50px;
-
-  .thumbnail-container {
-    height: 150px;
-
-    img {
-      height: 100%;
-    }
-  }
-
-
-  .link-container {
-    display: grid;
-    justify-content: center;
-    padding: 50px 0;
-
-    a {
-      text-decoration: none;
-      text-align: left;
-      display: block;
-      font-size: 18px;
-      color: #2c3e50;
-      padding: 10px 0;
-    }
-  }
-
-  .left-container-icon {
-
-    .media-icon {
-      padding: 10px;
-      cursor: pointer;
-    }
-  }
-}
-
 .right-container {
   padding-left: 20px;
   margin: 50px 0;
